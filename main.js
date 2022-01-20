121ed8ad3ea4d4d688e36a908d778b05924f98fb
Shamshad Abdullah <abdullas@clarkson.edu>
2022-01-20 15:01:59 -0500

    edits

---
+++ main.js
@@ -0,0 +1,11 @@
+function generate() {
+  let ranNum = Math.floor(Math.random() * quoteArr.length);
+  let lineVar = document.querySelector("blockquote");
+  let movieVar = document.querySelector("cite");
+  lineVar.innerText = quoteArr[ranNum].line;
+  movieVar.innerText = quoteArr[ranNum].movie;
+
+
+
+
+}
---
+++ quotes.js
@@ -0,0 +1,18 @@
+const quoteArr = [
+  {
+    "line": "hahahahahaha",
+    "movie": "Me"
+  },{
+    "line":"Spider Man is coming home",
+    "movie":"Spider Man"
+  },{
+    "line":"I am Groot",
+    "movie":"Guardians of The Galaxy"
+  },{
+    "line":"Make him an offer he can't refuse",
+    "movie":"Godfather"
+  },{
+    "line":"Hahahaha",
+    "movie":"Boo"
+  }
+]
