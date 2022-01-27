324344e7ca3462b53f3a516a2e404c9d828435ef
Shamshad Abdullah <abdullas@clarkson.edu>
2022-01-27 16:26:38 -0500

    done

--- audio.js
+++ audio.js
@@ -3,12 +3,12 @@ $(document).ready(function() {
   var audio  = new Audio('https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/click.mp3');
   var audio2 = new Audio('https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/clickUp.mp3')
 
-  $(".button").mousedown(function() {
+  $("button").mousedown(function() {
     audio2.load();
     audio2.play();
   });
 
-  $(".button").mouseup(function() {
+  $("button").mouseup(function() {
     audio.load();
     audio.play();
   });
